6f568aae995af4f6de3bf4aadc968d8f25cc31c2
k22036 <k22036kk@aitech.ac.jp>
2025-12-30 15:16:52 +0900

    chore(config): プロジェクトの設定の見直し

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "*.thecatapi.com",
       },
+      {
+        protocol: "https",
+        hostname: "*.tumblr.com",
+      },
     ],
   },
 };
